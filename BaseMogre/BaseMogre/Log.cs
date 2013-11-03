dd708c3742abd2777aa9a7e8389707a34c8a720f
Morgan <morgan.denis.60@gmail.com>
2013-11-03 11:07:10 +0100

    methode de log
    bon, j'ai fait 2 méthodes pour écrire dans un fichier de log (je sais
    pas si c'est ce a quoi tu pensais, mais au pire c'est toujours utile)
    Elle a une surcharge permettant de passer une ou N chaines dans un
    tableau (je pourrais mettre une liste au pire)

---
+++ BaseMogre/BaseMogre/Log.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace BaseMogre
+{
+    public static class Log
+    {
+        static string log_path = "resultats.log";
+        public static bool writeNewLine(string texte)
+        {
+            try
+            {
+                   File.AppendAllText(log_path, DateTime.Now.ToString() + " => " + texte + Environment.NewLine);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+        public static bool writeNewLine(string[] textes)
+        {
+            string chaine = DateTime.Now.ToString() + " => " + Environment.NewLine;
+            try
+            {
+                foreach (string S in textes)
+                {
+                    chaine += S + Environment.NewLine;
+                }
+                File.AppendAllText(log_path, chaine);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
