51cc4900b3ac229e30b2385e3d6eb84094725dca
Nicolas Drufin <nicolas.drufin@ensc.fr>
2013-12-15 23:10:46 +0100

    Rajout de déco pour le terrain

--- BaseMogre/BaseMogre/Map.cs
+++ BaseMogre/BaseMogre/Map.cs
@@ -11,6 +11,10 @@ namespace BaseMogre
     {
         public const float ALIGNEMENTTERRAIN = -15;
 
+        private static int MUSHCOUNT = 0;
+
+        private static Random rnd = new Random(); 
+
         public static void CreationPlan(ref SceneManager scm)
         {
             scm.AmbientLight = ColourValue.White;
@@ -21,8 +25,30 @@ namespace BaseMogre
             Entity groundEnt = scm.CreateEntity("GroundEntity", "ground");
             scm.RootSceneNode.CreateChildSceneNode().AttachObject(groundEnt);
             groundEnt.SetMaterialName("Texture/Grass");
+
+            for (int i = 0; i <= 360; i += 10)
+            {
+                int id = rnd.Next(3) + 1;
+                float x = (float)System.Math.Cos(System.Math.PI * i / 180) * 1500;
+                float z = (float)System.Math.Sin(System.Math.PI * i / 180) * 1500;
+                createShrooms(ref scm, id, new Vector3(x, ALIGNEMENTTERRAIN, z));
+            }
         }
 
+        /// <summary>
+        /// Crée un champignon
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="position"></param>
+        public static void createShrooms(ref SceneManager scm, int index, Vector3 position)
+        {
+            //Création de l'Entity et du Scenenode à la position
+            Entity entity = scm.CreateEntity("Mushroom" + MUSHCOUNT, "shroom1_"+index+".mesh");
+            SceneNode node = scm.RootSceneNode.CreateChildSceneNode("Node_Mushroom" + MUSHCOUNT, position);
+            node.AttachObject(entity);
+            node.Scale(new Vector3(50, 50, 50));
 
+            MUSHCOUNT++;
+        }
     }
 }
